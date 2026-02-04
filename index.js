d1717f6b280543988e37b058410abd06abc1d85c
Rafael-Rodrigues09 <rafael.rodrigues.sousa012@gmail.com>
2026-02-04 13:35:31 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,12 +5,27 @@ botao.addEventListener('click', function(enviar){
 let numsujo = document.getElementById('num').value
 let msg = document.getElementById('msg').value
 let numlimpo = numsujo.replace(/\D/g, '')
-  let msgformatada = encodeURIComponent(msg)
-  let url = `https://wa.me/${numlimpo}?text=${msgformatada}`
+let msgformatada = encodeURIComponent(msg)
+let url = `https://wa.me/${numlimpo}?text=${msgformatada}`
+let resultado = document.getElementById('resultado')
+let linkfinal = document.getElementById('linkfinal')
+let botaocopiar = document.getElementById('copbotao')
   if(numlimpo.length < 10){
     alert('Por favor, digite um número válido com DDD (ex: 88988186734)')
     return
   }
-  window.open(url, '_blank')
-  
+  linkfinal.innerText = url
+  linkfinal.href = url
+  resultado.style.display = 'block'
+  botaocopiar.style.display = 'block'
 } )
+
+function copiar(){
+  let linkcop = document.getElementById('linkfinal').innerText
+  let botaocopiar = document.getElementById('copbotao')
+  navigator.clipboard.writeText(linkcop)
+  botaocopiar.innerText = "✅ Copiado!"
+  setTimeout(function(){
+    botaocopiar.innerText = 'Copiar'
+    }, 2000)
+}
